79318fd0df505ced2df852c01f215b4097af5cc4
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-09 10:10:46 +0100

    feat: Elastic.Apm.All package has been renamed in the agent

--- opbeans-dotnet/Startup.cs
+++ opbeans-dotnet/Startup.cs
@@ -37,7 +37,7 @@ namespace OpbeansDotnet
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
-			app.UseElasticApm(Configuration);
+			app.UseAllElasticApm(Configuration);
 
 			Mapper.Initialize(cfg =>
 			{
