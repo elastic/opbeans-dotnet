11695471e3624f3f8ece4b4e11acb8859ad52f40
Victor Martinez <victormartinezrubio@gmail.com>
2019-11-29 14:28:54 +0000

    ci: release process (#21)

---
+++ .ci/bump-version.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+AGENT_VERSION="${1?Missing the APM dotnet agent version}"
+
+## Use docker to bump the version to ensure the environment is easy to reproduce.
+docker run --rm -t \
+  --user $UID \
+  -w /app \
+  -e HOME=/tmp \
+  -e AGENT_VERSION="${AGENT_VERSION}" \
+  -v "$(pwd)/opbeans-dotnet:/app" \
+  mcr.microsoft.com/dotnet/core/sdk:2.2 /bin/sh -c "
+    dotnet add package Elastic.Apm.NetCoreAll -v ${AGENT_VERSION}"
+
+# Commit changes
+git add opbeans-dotnet/opbeans-dotnet.csproj
+git commit -m "Bump version ${AGENT_VERSION}"
